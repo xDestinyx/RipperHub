f2acb5edaf5baec9511f045ce598112de9768037
xDestinyx <92421013+xDestinyx@users.noreply.github.com>
2022-11-07 19:59:05 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/xDestinyx/RipperHub/main/BF.lua'))();
+else
+    game.Players.LocalPlayer:Kick('this game not support')
+end
