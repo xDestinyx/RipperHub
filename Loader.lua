2bdeaa5036662670c81284777cbc9be16443b73b
xDestinyx <92421013+xDestinyx@users.noreply.github.com>
2022-11-27 21:39:20 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,3 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/xDestinyx/RipperHub/main/RipperHubV3.lua'))();
-else
-    game.Players.LocalPlayer:Kick('this game not support');
 end
