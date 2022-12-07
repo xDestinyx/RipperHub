3928cfea3946cf7dc1e8aec0b01e430488777ded
xDestinyx <92421013+xDestinyx@users.noreply.github.com>
2022-12-07 13:11:53 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/xDestinyx/RipperHub/main/RipperHubV3.lua'))();
+elseif game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/xDestinyx/RipperHub/main/KL.lua'))();
 elseif game.PlaceId == 855499080 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xDestinyx/RipperHub/main/SkyWars.lua"))();
 end
